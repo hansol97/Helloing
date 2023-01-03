322c76874f638298d0ff3894961285ac3c8b1aef
HANSOL <109151525+hansol97@users.noreply.github.com>
2023-01-03 19:37:43 +0900

    Update LoginInterceptor.java

--- Helloing/src/main/java/com/jl/helloing/common/interceptor/LoginInterceptor.java
+++ Helloing/src/main/java/com/jl/helloing/common/interceptor/LoginInterceptor.java
@@ -10,18 +10,6 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter{
     
-    /*
-     * extends HandlerInterceptorAdapter 상속받기
-     * 
-     * Interceptor(정확히 표현하면 HandlerInterceptor)
-     * 
-     * Controller가 실행되기 전, 실행된 후에 낚아채서 실행할 내용을 작성 가능
-     * 로그인 유무 판단, 회원의 권한 체크
-     * 
-     * preHandle(전처리) : 핸들러 호출 전 낚아챔
-     * postHandle(후처리) : 요청 처리 후 DispatcherServlet이 View 정보를 받기 전 낚아챔
-     */
-    
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         
