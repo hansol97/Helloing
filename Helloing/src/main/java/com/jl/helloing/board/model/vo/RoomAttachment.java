0e2862ccaf0dfca85aaf21ce8896ec1a3e28fcd8
hyejin0129 <121650451+hyejin0129@users.noreply.github.com>
2023-01-04 14:09:45 +0900

    RoomAttachment

---
+++ Helloing/src/main/java/com/jl/helloing/board/model/vo/RoomAttachment.java
@@ -0,0 +1,14 @@
+package com.jl.helloing.board.model.vo;
+
+import lombok.Data;
+
+@Data
+public class RoomAttachment {
+
+	private int fileNo;
+	private int accommNo;
+	private String originName;
+	private String changeName;
+	private String filePath;
+	private String uploadDate;
+}
