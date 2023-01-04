8f98eddd723f77537401f2feae58c162abfa94a4
hdy2961 <109939960+hdy2961@users.noreply.github.com>
2023-01-04 13:58:47 +0900

    Daeyoung
    gg

---
+++ Helloing/src/main/java/com/jl/helloing/admin/model/vo/Chatbot.java
@@ -0,0 +1,7 @@
+package com.jl.helloing.admin.model.vo;
+
+public class Chatbot {
+
+	private String chatbotQ; // "CAHTBOT_Q"	VARCHAR2(100)		PRIMARY KEY,
+	private String chatbotA; // "CAHTBOT_A"	VARCHAR2(1000)		NOT NULL
+}
