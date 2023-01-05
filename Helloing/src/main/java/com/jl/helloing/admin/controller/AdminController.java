91ee763fbbfa6b4b07be4e263e28e157dea372e0
chlwjdqls <121650415+chlwjdqls@users.noreply.github.com>
2023-01-05 14:35:52 +0900

    관리자 컨트롤러 수정 - 정빈

--- Helloing/src/main/java/com/jl/helloing/admin/controller/AdminController.java
+++ Helloing/src/main/java/com/jl/helloing/admin/controller/AdminController.java
@@ -10,5 +10,50 @@ public class AdminController {
 	public String adminPage() {
 		return "admin/menubar_admin";
 	}
+	
+	@RequestMapping("actPay.ad")
+	public String activityPaymentViewList() {
+		return "temp/activityPaymentListView";
+	}
+	
+	@RequestMapping("businessList.ad")
+	public String adminBusinessListView() {
+		return "temp/adminBusinessListView";
+	}
+	
+	@RequestMapping("boardList.ad")
+	public String adminBoardListView() {
+		return "temp/adminBoardListView";
+	}
+	
+	@RequestMapping("memList.ad")
+	public String adminMemberListView() {
+		return "temp/adminMemberListView";
+	}
+	
+	@RequestMapping("replyList.ad")
+	public String adminReplyListView() {
+		return "temp/adminReplyListView";
+	}
+
+	@RequestMapping("businessPayList.ad")
+	public String businessPaymentListView() {
+		return "temp/businessPaymentListView";
+	}
+	
+	@RequestMapping("chatBotList.ad")
+	public String chatBotListView() {
+		return "temp/chatBotListView";
+	}
+	
+	@RequestMapping("reportList.ad")
+	public String reportListView() {
+		return "temp/reportListView";
+	}
+	
+	@RequestMapping("roomPay.ad")
+	public String roomPaymentListView() {
+		return "temp/roomPaymentListView";
+	}
 
 }
