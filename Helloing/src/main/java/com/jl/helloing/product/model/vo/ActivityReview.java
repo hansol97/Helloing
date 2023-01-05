81e8f49990cc88a992f0a162d6cb41ca53b8b864
hyejin0129 <121650451+hyejin0129@users.noreply.github.com>
2023-01-05 10:42:30 +0900

    Update ActivityReview.java

--- Helloing/src/main/java/com/jl/helloing/product/model/vo/ActivityReview.java
+++ Helloing/src/main/java/com/jl/helloing/product/model/vo/ActivityReview.java
@@ -5,5 +5,12 @@ import lombok.Data;
 @Data
 public class ActivityReview {
 
-	
+	private int reviewNo;
+	private int activityNo;
+	private int ticketNo;
+	private int memNo;
+	private String reviewContent;
+	private String createDate;
+	private String tag;
+	private int star;
 }
