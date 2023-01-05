0a4a48acfdf2eae318dd5b3082a8d01cd52e33c1
chlwjdqls <121650415+chlwjdqls@users.noreply.github.com>
2023-01-05 14:34:44 +0900

    관리자 메뉴바 수정 - 정빈

--- Helloing/src/main/java/com/jl/helloing/admin/controller/AdminController.java
+++ Helloing/src/main/java/com/jl/helloing/admin/controller/AdminController.java
@@ -1,8 +1,14 @@
 package com.jl.helloing.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
+	
+	@RequestMapping("page.ad")
+	public String adminPage() {
+		return "admin/menubar_admin";
+	}
 
 }
