c38e9417a3bc0ec11d08e470b066ccbba8a6daf7
winjun91 <121650370+winjun91@users.noreply.github.com>
2023-01-05 12:13:10 +0900

    vo -주승준-
    잘못된거 바꿈

--- Helloing/src/main/java/com/jl/helloing/board/model/vo/TripAttachment.java
+++ Helloing/src/main/java/com/jl/helloing/board/model/vo/TripAttachment.java
@@ -2,4 +2,12 @@ package com.jl.helloing.board.model.vo;
 
 public class TripAttachment {
 
+	private int fileNo; // FILE_NO	NUMBER
+	private int boardNo; // BOARD_NO	NUMBER
+	private String originName; // ORIGIN_NAME	VARCHAR2(100 BYTE)
+	private String changeName; // CHANGE_NAME	VARCHAR2(100 BYTE)
+	private String filePath; // FILE_PATH	VARCHAR2(1000 BYTE)
+	private String uploadDate; // UPLOAD_DATE	DATE
+	
+	
 }
