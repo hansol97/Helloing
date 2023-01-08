76cb056e0b7403141b85743d781c0bfff8e40b16
winjun91 <121650370+winjun91@users.noreply.github.com>
2023-01-08 10:55:47 +0900

    Update MemberServiceImpl.java

--- Helloing/src/main/java/com/jl/helloing/member/model/service/MemberServiceImpl.java
+++ Helloing/src/main/java/com/jl/helloing/member/model/service/MemberServiceImpl.java
@@ -1,11 +1,20 @@
 package com.jl.helloing.member.model.service;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jl.helloing.member.model.dao.MemberDao;
 import com.jl.helloing.member.model.vo.Member;
 
 @Service
 public class MemberServiceImpl implements MemberService{
+	
+	@Autowired
+	private MemberDao memberDao;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
 	
 	// 로그인
 	@Override
