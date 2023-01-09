89e459c1118fd850dea49a640a1218b139ff7ef4
majoryoo <71409859+majoryoo@users.noreply.github.com>
2023-01-09 19:52:32 +0900

    Revert "AccommAttachment VO"
    This reverts commit 1f45dfd170f0ce9f873d2cdbbb6e0313f8182bb2.

--- Helloing/src/main/java/com/jl/helloing/product/model/vo/AccommAttachment.java
+++ Helloing/src/main/java/com/jl/helloing/product/model/vo/AccommAttachment.java
@@ -1,14 +1,5 @@
 package com.jl.helloing.product.model.vo;
 
-import lombok.Data;
-
-@Data
 public class AccommAttachment {
 
-	private int fileNo;	//	FILE_NO	NUMBER
-	private int accommNo;	//	ACCOMM_NO	NUMBER
-	private String originName;	//	ORIGIN_NAME	VARCHAR2(100 BYTE)
-	private String changeName;	//	CHANGE_NAME	VARCHAR2(100 BYTE)
-	private String filePath;	//	FILE_PATH	VARCHAR2(1000 BYTE)
-	private String uploadDate;	//	UPLOAD_DATE	DATE
 }
