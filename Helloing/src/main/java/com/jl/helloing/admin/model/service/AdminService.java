9ca9831ba30f05111243561829f2fe0266b6974a
chlwjdqls <121650415+chlwjdqls@users.noreply.github.com>
2023-01-09 14:45:09 +0900

    챗봇 목록 조회 -정빈

--- Helloing/src/main/java/com/jl/helloing/admin/model/service/AdminService.java
+++ Helloing/src/main/java/com/jl/helloing/admin/model/service/AdminService.java
@@ -3,6 +3,7 @@ package com.jl.helloing.admin.model.service;
 import java.util.ArrayList;
 
 import com.jl.helloing.admin.model.vo.Chatbot;
+import com.jl.helloing.common.model.vo.PageInfo;
 
 public interface AdminService {
 	
@@ -10,8 +11,11 @@ public interface AdminService {
 	// 챗봇 등록
 	int insertChatbot(Chatbot c);
 	
+	// 챗봇 키워드 수 조회
+	int selectChatbotListCount();
+	
 	// 챗봇 리스트 조회
-	ArrayList<Chatbot> selectChatbotList();
+	ArrayList<Chatbot> selectChatbotList(PageInfo pi);
 	
 	// 챗봇 수정 모달
 	Chatbot updateFormChatbot(String chatbotQ);
