ac2d0e186569bcbad7072984c95fabde1c4cb0d9
majoryoo <71409859+majoryoo@users.noreply.github.com>
2023-01-10 14:00:25 +0900

    점심먹은 기념

--- Helloing/src/main/java/com/jl/helloing/product/model/vo/Activity.java
+++ Helloing/src/main/java/com/jl/helloing/product/model/vo/Activity.java
@@ -7,7 +7,7 @@ public class Activity {
 
 	private int activityNo;
 	private String activityName;
-	private int businessNo;
+	private String businessNo;
 	private String activityContent;
 	private String endDate;
 	
