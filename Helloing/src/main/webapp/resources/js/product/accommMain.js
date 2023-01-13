13cf7471d0b2b37b57ea56f92247f3807e8dca06
HANSOL <109151525+hansol97@users.noreply.github.com>
2023-01-13 11:26:20 +0900

    숙소 메인 완료 - 한솔

--- Helloing/src/main/webapp/resources/js/product/accommMain.js
+++ Helloing/src/main/webapp/resources/js/product/accommMain.js
@@ -2,7 +2,7 @@ $(function(){
 	// 상품 div 클릭 시 상세보기 페이지로 이동
 	$(document).on('click', '.productbox, .accommCookie', function(){
 		var $accommNo = $(this).children().eq(0).val();
-		//console.log($accommNo);
+		console.log($accommNo);
 		location.href = "detail.accomm?accommNo=" + $accommNo; // 나중에 겟방식 url로 바꾸기
 	})
 })
