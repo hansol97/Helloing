35c29022082c0f36e4c7316b240c0f19ee6be9a0
HANSOL <109151525+hansol97@users.noreply.github.com>
2023-01-16 20:43:21 +0900

    메인페이지들 수정~

--- Helloing/src/main/webapp/resources/js/product/activityMain.js
+++ Helloing/src/main/webapp/resources/js/product/activityMain.js
@@ -1,4 +1,7 @@
 $(function(){
+
+	nameSplit();
+
 	// 상품 div 클릭 시 상세보기 페이지로 이동
 	$(document).on('click', '.productbox', function(){
 		var $activityNo = $(this).children().eq(0).val();
@@ -6,3 +9,14 @@ $(function(){
         location.href = "detail.activity?activityNo=" + $activityNo; // 나중에 겟방식 url로 바꾸기
 	})
 })
+
+// 숙소 이름이 23글자 이상이면 뒤에 .. 붙이기
+function nameSplit(){
+	$('.productbox').each(function(){
+		if($(this).find('.activityName').text().length >= 23){
+			var newName = $(this).find('.activityName').text().substr(0, 23) + "...";
+
+			$(this).find('.activityName').text(newName);
+		}
+	})
+}
